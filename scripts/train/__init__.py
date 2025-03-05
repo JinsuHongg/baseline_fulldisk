40945375637d1c7cdada193005c2f000b7a67a4e
Jinsu Hong <jinsuhong.knight@gmail.com>
2025-03-04 19:20:48 -0500

    update formatting

--- scripts/train/__init__.py
+++ scripts/train/__init__.py
@@ -2,4 +2,4 @@ from .dataloader import SolarFlSets
 from .evaluation import HSS2, TSS, F1Pos
 from .traintestloop import train_loop, test_loop
 
-__all__ = ['SolarFlSets', 'HSS2', 'TSS', 'F1Pos', 'train_loop', 'test_loop']
+__all__ = ["SolarFlSets", "HSS2", "TSS", "F1Pos", "train_loop", "test_loop"]
